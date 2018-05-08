ba4515005f4617dcc95c274ac216b4b9d808d438
daphne <d.van.ginneken@hotmail.com>
2018-05-08 13:19:34 +0200

    main

--- bstopopd/src/main.java
+++ bstopopd/src/main.java
@@ -1,7 +1,7 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class bstop extends Application {
+public class main extends Application {
 
     public static void main(String[] args) {
         launch(args);
